9791297bfc7670187b337b4db4ab0fd199057055
Ayaan <ayaan.panda1@gmail.com>
2020-08-03 22:44:12 -0400

    Making a few changes to model

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ words = []
 labels = []
 docs_x = []
 docs_y = []
+
 for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
@@ -24,12 +25,14 @@ for intent in data["intents"]:
         docs_y.append(intent["tag"])
     if intent["tag"] not in labels:
         labels.append(intent["tag"])
+
 words = [stemmer.stem(w.lower()) for w in words if w != "?"]
 words = sorted(list(set(words)))
 labels = sorted(labels)
 training = []
 output = []
 out_empty = [0 for _ in range(len(labels))]
+
 for x, doc in enumerate(docs_x):
     bag = []
     wrds = [stemmer.stem(w.lower()) for w in doc]
@@ -55,5 +58,5 @@ model = Sequential([
     Dense(len(output[0]), activation="softmax")
 ])
 model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
-model.fit(training, output, epochs=1000, batch_size=128)
+model.fit(training, output, epochs=1000, batch_size=256)
 model.save("model.h5")
